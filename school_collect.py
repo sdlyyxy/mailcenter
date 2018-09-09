b1acaecc5b5a438da1b6f87f0829d4c6b6a5a922
sdlyyxy <sdlyyxy@icloud.com>
2018-09-09 12:46:12 +0800

    Fix inform in first page

--- school_collect.py
+++ school_collect.py
@@ -4,6 +4,8 @@ import urllib.request
 import json
 import datetime
 import time
+import re
+
 
 'todo: change day delta(4 places)'
 
@@ -12,14 +14,25 @@ url = 'https://webapp.bupt.edu.cn/extensions/wap/news/get-list.html?p=%s&type=tz
 ress = "<h2>"+ (datetime.datetime.now() -
                    datetime.timedelta(days=1)).strftime('%Y年%m月%d日')+"通知公告</h2>\n</br>"
 
-def getInform(i):
+def getInform(i):    
     ress=''
     _url = url % i
+    if i==1:
+        _url='https://webapp.bupt.edu.cn/extensions/wap/news/list.html?type=tzgg'
+    # print(_url)
     datepattern = (datetime.datetime.now() -
                    datetime.timedelta(days=1)).strftime('%Y年%m月%d日')
     # print(datepattern)
     response = urllib.request.urlopen(_url)
     s = str(response.read(), encoding="utf8")
+    if i==1:
+        res_tr = r'<body >(.*?)</body>'
+        m_tr =  re.findall(res_tr,s,re.S|re.M)
+        ress=m_tr[0]
+        # print(ress)
+        ress=ress.replace('/extensions/wap/news/detail.html','https://webapp.bupt.edu.cn//extensions/wap/news/detail.html')
+        # print(ress)
+        return ress
     data = json.loads(s)
     # print(data['data'].keys())
     if datepattern in data['data'].keys():
