aeaca7ee18e22348288bcbc0770735fe6186fab6
sdlyyxy <sdlyyxy@icloud.com>
2019-03-12 10:28:43 +0800

    qlwb auto retry

--- qlwb_html.py
+++ qlwb_html.py
@@ -2,11 +2,30 @@
 
 import urllib2,os,re,urllib,time
 from lxml import etree
+
+def gethtml(r):
+    attempts = 0
+    success = False
+    res=''
+    while attempts < 5 and not success:
+        time.sleep(0.5)
+        try:
+            res=urllib2.urlopen(r,timeout=5).read()
+            success = True
+        except:
+            attempts += 1
+    return res
+
+
 index='http://epaper.qlwb.com.cn/qlwb/content/'
 header='Mozilla/5.0 (iPad; U; CPU OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5'
 request = urllib2.Request(index)
 request.add_header('User-Agent', header)
-html = urllib2.urlopen(request).read()
+# html = urllib2.urlopen(request,timeout=5).read()
+html=gethtml(request)
+if html=='':
+    print("get 'http://epaper.qlwb.com.cn/qlwb/content/' error!!!!!!!!!")
+    exit()
 pattern=re.compile(r'<A HREF="/qlwb/content/(........)/">........</A><br>')#why received html label is uppercase?
 alldates=pattern.findall(html)
 lastdate=len(alldates)-1 # An integer
@@ -53,7 +72,11 @@ finalhtml='''
 for s in d_match:
     s_request=urllib2.Request(index+s)
     s_request.add_header('User-Agent',header)
-    s_html=urllib2.urlopen(s_request).read()
+    # s_html=urllib2.urlopen(s_request).read()
+    s_html=gethtml(s_request)
+    if s_html=='':
+        print("get single page %s error!!!!!!!!!",index+s)
+        exit()
     # s_pattern=re.compile(r'(<div class="fbt">.*<div class="fbt">.*</div>)')
     # s_match=s_pattern.findall(s_html)
     # for item in s_match:
