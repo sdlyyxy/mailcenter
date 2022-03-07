563d25e0b2d5cdf84272d77377e0161970f6a32c
sdlyyxy <sdlyyxy@icloud.com>
2022-03-07 13:01:37 +0800

    Add feature: show retweet content

--- get_weibo.py
+++ get_weibo.py
@@ -16,7 +16,7 @@ def getWeiboByID(userid):
     res=res+u"Ta关注的用户数：{}".format(p.follow_count)+"</br>"
     res=res+u"关注Ta的用户数：{}".format(p.followers_count)+"</br></br>"
     res=res+u"Ta最近发布的："+"</br>"
-    res=res+"=================================================="+"</br>"
+    res=res+"</br>"
     for status in p.statuses.page(1):    
         dt = datetime.datetime.strptime(status.created_at, "%a %b %d %H:%M:%S %z %Y")
         if dt.date()>today:
@@ -29,8 +29,15 @@ def getWeiboByID(userid):
         for url in status.pic_urls:
             res+='<img src="%s" alt="Weibo Image Cannot be loaded..."></br></br>'%url
             # res+='<div style="width:240"><img src="%s" alt="Weibo Image Cannot be loaded..." style="width:100%%;height:auto"></div></br></br>'%url
+        if status.retweeted_status:
+            cl = WeiboClient() 
+            p = cl.status(status.retweeted_status.get("id"))
+            res=res+'<a href="https://m.weibo.cn/detail/%s">原推：%s：</a>%s</br>'%(status.retweeted_status.get("id"),status.retweeted_status.get("user").get("screen_name"),p.longTextContent)
+            for pic in status.retweeted_status.get('pics', []):
+                res+='<img src="%s" alt="Weibo Image Cannot be loaded..."></br></br>'%(pic.get('url'))
+
         res+="转发：%s  评论：%s 点赞：%s</br>"%(status.reposts_count,status.comments_count,status.attitudes_count)
-        res=res+"=================================================="+"</br>"
+        res=res+"</br></br></br>"
     res+="</br></br></br>"
 
     return res
