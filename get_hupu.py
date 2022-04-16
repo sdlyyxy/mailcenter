46335f4d43fa15fd4d32302c969fbd2120e04546
sdlyyxy <sdlyyxy@icloud.com>
2022-04-16 10:29:33 +0800

    Add auto retry for hupu

--- get_hupu.py
+++ get_hupu.py
@@ -1,12 +1,22 @@
+import urllib
+from tenacity import retry
+from tenacity import stop_after_attempt
+from tenacity import wait_fixed
 from urllib.request import urlopen
 import json
+
+
+@retry(stop=stop_after_attempt(7),wait=wait_fixed(2))
+def urlopen_with_retry(urlstr):
+    return urlopen(urlstr)
+
 
 res=""
 
 url = "https://m.hupu.com/api/v2/home-feed/%d"
   
 for i in range(1,3+1):
-    response = urlopen(url%i)
+    response = urlopen_with_retry(url%i)
     data_json = json.loads(response.read())
     for j in data_json["data"]["list"]:
         res+='%s：<a href="%s">链接</a></br></br>'%(j["title"],j["url"])
